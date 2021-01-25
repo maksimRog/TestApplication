0b196b1863611976623704527ae04114c6e6d514
MaksRog <mexarms@gmail.com>
2021-01-25 15:19:46 +0300

    first screen has beed ended.

--- app/src/main/java/com/mraha/weatherforecast/fragments/TodayFragment.java
+++ app/src/main/java/com/mraha/weatherforecast/fragments/TodayFragment.java
@@ -30,15 +30,12 @@ import java.util.Locale;
 public class TodayFragment extends Fragment implements FragmentView {
     private TextView locationTV;
     private Context context;
-
-
-    @Override
-    public void onAttach(Context context) {
-        super.onAttach(context);
-        ((MainActivity) context).setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
-        this.context = context;
-    }
-
+    private TextView humidityTV;
+    private TextView pressureTV;
+    private TextView speedTV;
+    private TextView directionTV;
+    private TextView precipitationTV;
+    private TextView degreesTV;
 
     @Nullable
     @Override
@@ -49,13 +46,65 @@ public class TodayFragment extends Fragment implements FragmentView {
     @Override
     public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
+        humidityTV = view.findViewById(R.id.humidity);
+        pressureTV = view.findViewById(R.id.pressure);
+        precipitationTV = view.findViewById(R.id.precipitation);
+        speedTV = view.findViewById(R.id.wind_speed);
+        directionTV = view.findViewById(R.id.wind_direction);
+        degreesTV = view.findViewById(R.id.degrees);
         locationTV = view.findViewById(R.id.location);
         initViews();
-
     }
 
     @Override
     public void initViews() {
+        CurrentMain currentMain = Repository.getCurrentData();
+        if (currentMain != null) {
+            setCurrentLocation();
+            degreesTV.setText(currentMain.getWeather().get(0).getDescription());
+            humidityTV.setText(currentMain.getMain().getHumidity());
+            pressureTV.setText(currentMain.getMain().getPressure());
+            if (currentMain.getRain() == null) {
+                precipitationTV.setText(getString(R.string.precip_def_value));
+            } else {
+                precipitationTV.setText(currentMain.getRain().generatePrecipitationString());
+            }
+            speedTV.setText(currentMain.getWind().getSpeed());
+            directionTV.setText(currentMain.getWind().getDeg() + currentMain.getWind().getWindDirection());
+            switch (currentMain.getWeather().get(0).getMain().toLowerCase()) {
+                case "thunderstorm":
+                    locationTV.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.storm, 0, 0);
+                    break;
+                case "clouds":
+                    locationTV.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.clouds, 0, 0);
+                    break;
+                case "rain":
+                    locationTV.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.rain, 0, 0);
+                    break;
+                case "snow":
+                    locationTV.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.snowing, 0, 0);
+                    break;
+                case "clear":
+                    locationTV.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.sun, 0, 0);
+                    break;
+                default:
+                    locationTV.setCompoundDrawablesWithIntrinsicBounds(0, R.drawable.foggy, 0, 0);
+                    break;
+            }
+
+        }
+    }
+
+
+    @Override
+    public void onAttach(Context context) {
+        super.onAttach(context);
+        ((MainActivity) context).setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        this.context = context;
+    }
+
+
+    public void setCurrentLocation() {
         MLocationListener mLocationListener = (MLocationListener) ((MainActivity) context).getLocationListener();
         List<Address> addresses;
         Geocoder geocoder = new Geocoder(context, Locale.getDefault());
